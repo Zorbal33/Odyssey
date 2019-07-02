94a40717e8db4a1fa3396b7bfc15263ba6fa44a0
flavien chappe <charly220v@hotmail.com>
2019-07-02 17:04:44 +0200

    formulaire

--- front/src/SignUp.jsx
+++ front/src/SignUp.jsx
@@ -1,10 +1,38 @@
 import React, { Component } from 'react';
 
 class SignUp  extends Component {
+    constructor(props){
+    super(props);
+    this.state = {
+        email:"",
+        password:"",
+        passwordCheck:"",
+        firstname:"",
+        lastname:"",
+    }}
+
+    onChange = (e) => {
+        this.setState({
+            [e.target.name]: e.target.value,
+        });
+    }
+
     render() {
         return (
             <div>
-                <input type='email' name='email'/>
+                <h1>
+                email:{this.state.email} <br/>
+                password:{this.state.password} <br/>
+                passwordCheck:{this.state.passwordCheck} <br/>
+                prénom:{this.state.firstname} <br/>
+                nom:{this.state.lastname}<br/>
+                </h1>
+                
+                <input onChange={this.onChange} type="email" value={this.state.name} name="email" />
+                <input onChange={this.onChange} value={this.state.name} name="password" />
+                <input onChange={this.onChange} value={this.state.name} name="passwordCheck" />
+                <input onChange={this.onChange} value={this.state.name} name="firstname" />
+                <input onChange={this.onChange} value={this.state.name} name="lastname" />
             </div>
         );
     }
