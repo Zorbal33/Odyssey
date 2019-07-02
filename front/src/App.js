95ac717ce7a0502280067e2e4f214917d136c351
flavien chappe <charly220v@hotmail.com>
2019-07-02 16:04:45 +0200

    input email

--- front/src/App.js
+++ front/src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import SignUp  from './SignUp'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <SignUp />
     </div>
   );
 }
---
+++ front/src/SignUp.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class SignUp  extends Component {
+    render() {
+        return (
+            <div>
+                <input type='email' name='email'/>
+            </div>
+        );
+    }
+}
+
+export default SignUp ;
